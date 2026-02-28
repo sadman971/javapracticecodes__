93b8e564d726f93c79496296c3ab43ea37fb1ed9
Sadman Rafid <sadman.rafid971@gmail.com>
2026-02-28 15:11:54 +0600

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,11 @@
+//Printing 
+public class Assignment1 {
+    public static void main() {
+        System.out.println("I=D: 101");
+        System.out.println("Title: Iphone 15");
+        System.out.println("Price: 1895 euros");
+        System.out.println("Description: Perfect product with best image quality");
+        System.out.println("Category: Phone");
+    }
+
+}
