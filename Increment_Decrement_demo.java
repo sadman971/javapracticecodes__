7795de137598562363bb6e90df6421236b975a6b
Sadman Rafid <sadman.rafid971@gmail.com>
2026-02-28 22:03:09 +0600

    Add files via upload

---
+++ Increment_Decrement_demo.java
@@ -0,0 +1,21 @@
+public class Increment_Decrement_demo {
+    
+    public static void main(String[] args) {
+        
+        int x =30, y;
+
+        y = ++x; // prefix increment
+        System.out.println(" y = "+y + ", x = "+x); 
+
+        y = x++; // postfix increment
+        System.out.println(" y = "+y + ", x = "+x);
+
+        y = --x; // prefix decrement
+        System.out.println(" y = "+y + ", x = "+x); 
+
+        y = x--; // postfix decrement
+        System.out.println(" y = "+y + ", x = "+x);
+
+
+    }
+}
